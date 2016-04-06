fce9babdf334e8438b328e1f13aa046fac334e33
jaque220 <jaq34@yahoo.com>
2016-04-06 14:42:05 -0400

    cooollll commit

---
+++ Tamagotchi/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi
+{
+    class Punisher
+    {
+        private string name = "N/A";
+        private string profile = "N/A";
+        private int ammoRounds = 0;
+        private int happy = 0;
+
+        public Punisher(string n, string pro, int ar, int h)
+        {
+            name = n;
+            profile = pro;
+            ammoRounds = ar;
+            happy = h;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public string getProfile()
+        {
+            return profile;
+        }
+        public int getAmmoRounds()
+        {
+            return ammoRounds;
+        }
+        public int getHappy()
+        {
+            return happy;
+        }
+        public int reload()
+        {
+            ammoRounds = ammoRounds + 1;
+            return ammoRounds;
+        }
+        public int punishment()
+        {
+            happy = happy + 1;
+            return happy;
+        }
+        public void dostuff()
+        {
+            Random rnd = new Random();
+            happy = happy - rnd.Next(0, 12);
+            ammoRounds = ammoRounds - rnd.Next(10);
+            Console.WriteLine(happy);
+            Console.WriteLine(ammoRounds);
+           
+        }
+    }
+}
---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Punisher p1 = new Punisher("Frank Castle", "Killer", 100, 100);
+            Console.WriteLine("Name: {0}, Profile: {1}, ammoRounds: {2}, Happy: {3}", p1.getName(), p1.getProfile(), p1.getAmmoRounds(), p1.getHappy());
+            while (p1.getAmmoRounds() > 0 && p1.getHappy() > 0)
+            {
+                Console.WriteLine("Give me food or attention or....YOU WILL DIE!!");
+                string action = Console.ReadLine();
+                if (action == "reload")
+                {
+                    p1.reload();
+                    Console.WriteLine("I'm kinda, sorta, hungry...FEED ME FOOD! GOOD STUFF! NOWWWWWW!!!!! RAWRRRRR!!");
+                }
+               
+                else if (action == "punishment")
+                {
+                    p1.punishment();
+                    Console.WriteLine("Good job, i'll leave your tombstone blank.....for now.");
+
+                }
+                else
+                {
+                    Console.WriteLine("*gunshots fire rapidly*");
+                }
+                p1.dostuff();
+                    Console.WriteLine("Punisher's ammo is {0} and happiness is {1}", p1.getAmmoRounds(), p1.getHappy());
+                if (p1.getAmmoRounds() <= 0 || p1.getHappy() <= 0)
+                {
+                    Console.WriteLine("Here Lies Frank Castle");
+                }
+                
+                    
+            }
+        }
+    }   
+} 
